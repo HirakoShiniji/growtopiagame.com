d0cbbc7df3b316e58f4f4afb610e4d1fd543950f
Liab-1337 <43638010+Liab-1337@users.noreply.github.com>
2019-04-11 13:01:59 +0300

    Create server_data.php

---
+++ server_data.php
@@ -0,0 +1,20 @@
+server|185.101.94.172
+
+port|17091
+
+type|1
+
+#maint|Mainetrance message (Not used for now) -- Growtopia Noobs
+
+
+
+beta_server|185.101.94.172
+
+beta_port|17091
+
+
+
+beta_type|1
+
+meta|localhost
+RTENDMARKERBS1001
